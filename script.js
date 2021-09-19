db04c187a12c0094f366a46154fd0d7c16307ce5
shinndeng <shinnmu@gmail.com>
2021-09-19 22:46:15 +1000

    separate pages

---
+++ script.js
@@ -0,0 +1,11 @@
+var x = document.getElementById("guide-bar");
+element.addEventListener("click", isClick);
+element.addEventListener("click", isSubmit);
+
+function isClick(){
+    alert("click");
+}
+
+function isSubmit(){
+    alert("Submission not available");
+}
